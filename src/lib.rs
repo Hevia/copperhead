1b7680b7656660aa18aa673388195941777af89a
Anthony Hevia <anthonyhevia@microsoft.com>
2021-03-19 20:54:13 -0400

    dev

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+use std::fs;
+use pyo3::prelude::*;
+use pyo3::wrap_pyfunction;
+
+/// Open a file & read a file as a string
+#[pyfunction]
+fn read_file(filename: String) -> PyResult<String>{
+     Ok(fs::read_to_string(filename).expect("Couldn't find file at path!"))
+}
+
+#[pymodule]
+fn moray_lib(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(read_file, m)?)?;
+    Ok(())
+}
